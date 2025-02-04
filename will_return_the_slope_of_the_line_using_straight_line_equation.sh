59586667cb2c5fbf5326db5bf586afa6cb7a9fde
sheetal <srana4226@conestoga.on.ca>
2025-02-04 14:53:29 -0500

    added the line of code to transfer the value of x, y and b to the slope line calculator file.

---
+++ will_return_the_slope_of_the_line_using_straight_line_equation.sh
@@ -0,0 +1,6 @@
+#will return the result for slope value from three given values x-coordinate, y-coordinate and the b-intersept point
+x=2
+y=2
+b=1
+RESULT=$(./slope_of_the_line_calculator_with_xyb_values.sh $x $y $b)
+
